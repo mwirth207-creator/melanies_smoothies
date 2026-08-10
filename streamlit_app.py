280aa27605c372a8c81df2b29f574c3dc7c2977f
mwirth207-creator <mwirth207@gmail.com>
2026-08-10 15:21:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,8 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+import request
+url = ['https://my.smoothiefroot.com/api/fruit/watermelon']
+smoothiefroot_response = requests.get(url[0])
+#smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response.json())
