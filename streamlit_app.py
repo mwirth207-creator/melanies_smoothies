a0660b6bf80ee920b8d52e3121ea7e35361a5400
mwirth207-creator <mwirth207@gmail.com>
2026-08-10 16:24:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,14 +29,17 @@ ingredients_list = st.multiselect(
     )
     
 ingredients_string = ''    
+#url = ['https://my.smoothiefroot.com/api/fruit/']
+url = "https://my.smoothiefroot.com/api/fruit/"
 
 if ingredients_list: 
 
   for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        url = ['https://my.smoothiefroot.com/api/fruit/']
-        smoothiefroot_response = requests.get(url[0] + fruit_chosen)
+        st_url = url + fruit_chosen
+        st.write (st_url)
+        smoothiefroot_response = requests.get(url)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         st.text(ingredients_string)
