fad559a3491e37c7b71fb5013044ddd1adb91021
mwirth207-creator <mwirth207@gmail.com>
2026-08-10 15:26:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,4 @@ import requests
 url = ['https://my.smoothiefroot.com/api/fruit/watermelon']
 smoothiefroot_response = requests.get(url[0])
 #smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_containter_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
