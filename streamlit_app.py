9e7cebba4fc9c70e633588e7b79f88795eaa8d75
mwirth207-creator <mwirth207@gmail.com>
2026-08-10 16:26:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,11 +48,11 @@ if ingredients_list:
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
         
-        time_to_insert = st.button('Submit Order')
+time_to_insert = st.button('Submit Order')
 
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
     
 
