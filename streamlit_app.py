fe862feff949eab60a2ae82e1027d18ec7758fed
mwirth207-creator <mwirth207@gmail.com>
2026-08-11 09:43:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list:
         
         #encoded_data = requests.models.RequestEncodingMixin._encode_params(search_on.strip())
     
-        st_url = "https://my.smoothiefroot.com/api/fruit/" + search_on.strip().lower()
-        st_url = "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen.lower()
+        #st_url = "https://my.smoothiefroot.com/api/fruit/" + search_on.strip().lower()
+        st_url = "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
         st.write (st_url)
         
         #### TEMP ####
